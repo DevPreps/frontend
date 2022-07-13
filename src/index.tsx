b9ab29f53321b26d112b9a2dc872738343f74aa6
b08waffles <b08waffles@protonmail.com>
2022-07-13 15:12:31 +1000

    Add Jest + Enzyme test suite and boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// This is the way to render one's react app when using
+// react v17 with react-dom v17 and react-router-dom v6.
+// Different version of those 3 packages may require
+// alterations to this code to work.
+
+ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <Routes>
+                <Route path="*" element={<App />} />
+            </Routes>
         </BrowserRouter>
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById("root")
 );

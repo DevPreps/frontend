e6edab85d30e842cac87d6bd7faf29d8960ef7cf
Lisa Li <lisali@192-168-1-103.tpgi.com.au>
2022-07-20 21:38:22 +1000

    fix: merge imports in myAccount.tsx

--- src/pages/MyAccount.tsx
+++ src/pages/MyAccount.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const MyAccount = () => {
     return (

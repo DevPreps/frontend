8244816c602496272a2766781611d9ca8b004a58
Lisa Li <lisali@192-168-1-103.tpgi.com.au>
2022-07-20 10:26:58 +1000

    refactor: add index route and delete compose route

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import {
 	AboutUs,
-	Compose,
 	Home,
 	MyAccount,
 	Login,
-	NewInterviewPost,
-	NewLearnPost,
-	NewProjectPost,
-	NewGeneralPost,
 	NotFound,
 	Post,
 	PostList,
@@ -18,33 +13,25 @@ import {
 
 export default function App() {
 	return (
-		<>
-			<Routes>
-				{/* Navbar and Footer Component will be inside Home component */}
-				<Route path="/" element={<Home />}>
-					<Route path="register" element={<Register />} />
-					<Route path="login" element={<Login />} />
-					<Route path="about-us" element={<AboutUs />} />
-					{/* side bar and personal info will be inside <MyAccount /> */}
-					<Route path="my-account" element={<MyAccount />}>
-						{/* These 3 post List could be a separate PostList component or use the same PostList component with interview/learn/projects/discussions */}
-						<Route path="my-posts" element={<PostList />} />
-						<Route path="my-bookmarks" element={<PostList />} />
-						<Route path="my-drafts" element={<PostList />} />
-					</Route>
-
-					<Route path="posts/:category" element={<PostList />} />
-					<Route path="posts/:category/:postid" element={<Post />} />
-					{/* the Compose component only contain section title and category options */}
-					<Route path="compose" element={<Compose />}>
-						<Route path="learn" element={<NewLearnPost />} />
-						<Route path="interview" element={<NewInterviewPost />} />
-						<Route path="project" element={<NewProjectPost />} />
-						<Route path="general" element={<NewGeneralPost />} />
-					</Route>
+		<Routes>
+			{/* Navbar and Footer Component will be inside Home component */}
+			<Route path="/" element={<Home />}>
+				{/* This will give us a component to render the home page content as the default. */}
+				<Route index element={<Home />} />
+				<Route path="register" element={<Register />} />
+				<Route path="login" element={<Login />} />
+				<Route path="about-us" element={<AboutUs />} />
+				<Route path="posts/:category" element={<PostList />} />
+				<Route path="posts/:category/:postid" element={<Post />} />
+				{/* side bar and personal info will be inside <MyAccount /> */}
+				<Route path="my-account" element={<MyAccount />}>
+					{/* These 3 post List could be a separate PostList component or use the same PostList component with interview/learn/projects/discussions */}
+					<Route path="my-posts" element={<PostList />} />
+					<Route path="my-bookmarks" element={<PostList />} />
+					<Route path="my-drafts" element={<PostList />} />
 				</Route>
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</>
+			</Route>
+			<Route path="*" element={<NotFound />} />
+		</Routes>
 	);
 }

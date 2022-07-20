bec3a2a19c548cf49afc1916c54831c3db7ba1cd
Lisa Li <lisali@192-168-1-103.tpgi.com.au>
2022-07-20 22:39:40 +1000

    fix: merge conflicts in App.test.tsx

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,13 +2,8 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
 
-<<<<<<< HEAD
 describe("Test App Entry point", () => {
     it("should be able to render <App />", () => {
-=======
-describe("Test App Entry point", function () {
-    it("should be able to render <App />", function () {
->>>>>>> development
         shallow(<App />);
     });
 });

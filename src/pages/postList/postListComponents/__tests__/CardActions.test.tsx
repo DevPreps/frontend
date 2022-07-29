a33ccc67a727c9293d59b1c4e32f570242cc1afb
Lisa <2016lisali@gmail.com>
2022-07-29 15:23:10 +1000

    Feat: add favicon and change site tilte
    feat: add updateInterviewPost modal

---
+++ src/pages/postList/postListComponents/__tests__/CardActions.test.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { shallow } from "enzyme";
+import {AuthorCardActions, UserCardActions,} from "../CardActions";
+
+// test component rendering
+describe("<AuthorCardActions /> rendering", () => {
+    const wrapper = shallow(<AuthorCardActions />);
+    // test if AuthorCardActions component renders
+    it("should be able to render <AuthorCardActions />", () => {     
+        wrapper;
+    });
+
+    // test if UpdateGeneralPost Modal renders
+    it("should contain UpdateGeneralPost Modal />", () => {   
+        expect(wrapper.find("UpdateGeneralPost").length).toBe(1);
+    });
+});
+
+describe("<UserCardActions /> rendering", () => {
+    const wrapper = shallow(<UserCardActions />);
+    // test if UserCardActions component renders
+    it("should be able to render <UserCardActions />", () => {
+        wrapper;
+        console.log(wrapper.debug())
+    });
+});

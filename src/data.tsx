6a89792bb60a61d3d21d91d1c38af5ce8c43c47b
Lisa <2016lisali@gmail.com>
2022-07-30 17:39:13 +1000

    feat: add data.tsx in root folder to store category, tags, and positions value

---
+++ src/data.tsx
@@ -0,0 +1,9 @@
+// TODO these values can be stored in frontend but should be updated to be the same with database
+export const tagOptions = ["frontend", "backend", "fullstack", "database"];
+export const categoryOptions = ["learn", "interview", "project", "general"];
+export const positionOptions = [
+    "Front End Developer",
+    "Back End Developer",
+    "Full Stack Developer",
+    "Software Engineer",
+];

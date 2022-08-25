005b8f5bdb9b7d3a1bad158931985defda4273c1
Cristovao Galambos <galamboscristovao@gmail.com>
2022-08-25 21:59:41 +1000

    test: remove log

--- src/forms/__tests__/RichTextEditor.test.tsx
+++ src/forms/__tests__/RichTextEditor.test.tsx
@@ -5,8 +5,6 @@ import RichTextEditor from "../RichTextEditor";
 describe("<RichTextEditor />", () => {
     it("finds quill text editor", () => {
         const wrapper = mount(<RichTextEditor />);
-        console.log(wrapper.debug());
-
         expect(wrapper.find(".quill")).toHaveLength(1);
     });
 });

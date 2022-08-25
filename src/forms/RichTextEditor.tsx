af476b6613cffdb1c9dbc8bbbcce975a9457897f
Cristovao Galambos <galamboscristovao@gmail.com>
2022-08-25 16:28:48 +1000

    ref: run ci

--- src/forms/RichTextEditor.tsx
+++ src/forms/RichTextEditor.tsx
@@ -4,41 +4,55 @@ import "react-quill/dist/quill.snow.css";
 import "react-quill/dist/quill.bubble.css";
 
 const RichTextEditor = () => {
-
     const modules = {
         toolbar: [
             [{ size: [false, "large"] }],
             ["bold", "italic", "underline"],
             ["blockquote", "code-block"],
-            [{ list:  "ordered" }, { list:  "bullet" }],
-            [{ indent:  "-1" }, { indent:  "+1" }, { align: ["", "center", "right", "justify"] }],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [
+                { indent: "-1" },
+                { indent: "+1" },
+                { align: ["", "center", "right", "justify"] },
+            ],
             ["link", "image", "video"],
             ["clean"],
         ],
-    }
-  
-    const formats = [ 
-        "header", "size",
-        "bold", "italic", "underline",
-        "font", "size", 
-        "color", "background",
-        "blockquote", "code-block",
-        "list", "bullet", 
-        "indent","align",
-        "link", "image", "video",
-        "clean"
-    ]
+    };
+
+    const formats = [
+        "header",
+        "size",
+        "bold",
+        "italic",
+        "underline",
+        "font",
+        "size",
+        "color",
+        "background",
+        "blockquote",
+        "code-block",
+        "list",
+        "bullet",
+        "indent",
+        "align",
+        "link",
+        "image",
+        "video",
+        "clean",
+    ];
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [value, setValue] = useState("");
-    
-    return  <ReactQuill
-                modules={modules} 
-                formats={formats} 
-                theme="snow" 
-                onChange={setValue} 
-                placeholder="Post content" 
-            />;
 
-}
+    return (
+        <ReactQuill
+            modules={modules}
+            formats={formats}
+            theme="snow"
+            onChange={setValue}
+            placeholder="Post content"
+        />
+    );
+};
 export default RichTextEditor;
